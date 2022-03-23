fce533248bbcdb90fb1d93b2c1ead9c94480850b
Roberto <bkastro@gmail.com>
2022-03-23 17:45:58 -0300

    Ajustes na responsividade e na limpeza dos campos após carregamento dos cards

--- SuperHeroForm/script.js
+++ SuperHeroForm/script.js
@@ -1,11 +1,11 @@
 // recebe elemento formulario
-const form = document.querySelector('#form')
+const form = document.querySelector("#form");
 
 // recebe container onde serao adicionados os cards
-const containerCards = document.querySelector('#container-cards')
+const containerCards = document.querySelector("#container-cards");
 
 // lista que ira receber os objetos cards
-var cards = []
+var cards = [];
 
 // funcao que cria o template e adiciona o card ao container
 const addCard = function (nome, descricao, url) {
@@ -15,29 +15,34 @@ const addCard = function (nome, descricao, url) {
        <h1 class="heroName">${nome}</h1>
        <p class="description">${descricao}</p>
      </div>
-    `
+    `;
 
-  containerCards.innerHTML += template
-}
+  containerCards.innerHTML += template;
+};
 
 // event onsubmit - ao clicar botao de add imagens
 form.onsubmit = function (event) {
-  var nome = document.querySelector('#nome').value
-  var descricao = document.querySelector('#descricao').value
-  var url = document.querySelector('#url').value
+  var nome = document.querySelector("#nome").value;
+  var descricao = document.querySelector("#descricao").value;
+  var url = document.querySelector("#url").value;
 
   // criar o card ao clicar em submit
   var card = {
     nome: nome,
     descricao: descricao,
-    url: url
-  }
+    url: url,
+  };
 
   // adicionar o card à lista cards
-  cards.push(card)
+  cards.push(card);
 
   // executa funcao que add o card ao DOM
-  addCard(nome, descricao, url)
+  addCard(nome, descricao, url);
 
-  event.preventDefault()
-}
+  //Limpa os campos após o carregamento do card
+  nome = document.querySelector("#nome").value = "";
+  descricao = document.querySelector("#descricao").value = "";
+  url = document.querySelector("#url").value = "";
+
+  event.preventDefault();
+};
