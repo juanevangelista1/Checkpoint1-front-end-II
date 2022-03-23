f31b54e2a03a545da201b554934ed31bb029bb4d
juanevangelista1 <juan.evangelista.nascimentoo@gmail.com>
2022-03-22 23:50:37 -0300

    main

---
+++ SuperHeroForm/script.js
@@ -0,0 +1,52 @@
+// recebe elemento formulario
+const form = document.querySelector('#form')
+
+// recebe container onde serao adicionados os cards
+const containerCards = document.querySelector('#container-cards')
+
+// lista que ira receber os objetos cards
+var cards = []
+
+// funcao que cria o template e adiciona o card ao container
+const addCard = function (nome, descricao, url) {
+  const template = `
+    <div class="card">
+       <img src="${url}" alt="${nome}" >
+       <h1 class="heroName">${nome}</h1>
+       <p class="description">${descricao}</p>
+     </div>
+    `
+
+  containerCards.innerHTML += template
+}
+
+// event onsubmit - ao clicar botao de add imagens
+form.onsubmit = function (event) {
+  var nome = document.querySelector('#nome').value
+  var descricao = document.querySelector('#descricao').value
+  var url = document.querySelector('#url').value
+
+  // criar o card ao clicar em submit
+  var card = {
+    nome: nome,
+    descricao: descricao,
+    url: url
+  }
+
+  // adicionar o card à lista cards
+  cards.push(card)
+
+  // executa funcao que add o card ao DOM
+  addCard(nome, descricao, url)
+
+  event.preventDefault()
+}
+
+//exemplos de imagens para testes
+//https://picsum.photos/id/1024/400/300
+//https://picsum.photos/id/237/400/300
+//https://picsum.photos/id/1025/400/300
+//https://picsum.photos/id/1069/400/300
+//https://picsum.photos/id/1074/400/300
+//https://picsum.photos/id/1084/400/300
+//https://picsum.photos/id/169/400/300
