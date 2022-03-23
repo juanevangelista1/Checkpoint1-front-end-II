80cc466fab223558013507eeb5f31be0aebfa47d
juanevangelista1 <juan.evangelista.nascimentoo@gmail.com>
2022-03-23 00:19:51 -0300

    main

--- SuperHeroForm/script.js
+++ SuperHeroForm/script.js
@@ -41,12 +41,3 @@ form.onsubmit = function (event) {
 
   event.preventDefault()
 }
-
-//exemplos de imagens para testes
-//https://picsum.photos/id/1024/400/300
-//https://picsum.photos/id/237/400/300
-//https://picsum.photos/id/1025/400/300
-//https://picsum.photos/id/1069/400/300
-//https://picsum.photos/id/1074/400/300
-//https://picsum.photos/id/1084/400/300
-//https://picsum.photos/id/169/400/300
